6695cd77d1faa6ca4f208a4b23c1a373e0b5e2b7
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2023-05-14 01:47:59 +0900

    python-wsgi: test POST json

--- python-wsgi/mywsgi.py
+++ python-wsgi/mywsgi.py
@@ -1,13 +1,15 @@
 import urllib
+import json
 
 def application(environ, start_response):
-    method = environ.get('REQUEST_METHOD')
+    method = environ['REQUEST_METHOD']
     params = ""
     if method == 'GET':
         params = urllib.parse.parse_qsl(environ.get('QUERY_STRING'))
     elif method == 'POST':
-        #TODO json
-        pass
+        wsgi_input = environ["wsgi.input"]
+        content_length = int(environ["CONTENT_LENGTH"])
+        params = json.loads(wsgi_input.read(content_length))
 
     start_response('200 OK', [('Content-Type','text/html')])
-    return [("Hello world:" + method + ",params=" + str(params)).encode()]
+    return [("Hello world:" + method + ",params=" + str(params) + "\n").encode()]
--- python-wsgi/test_mywsgi.py
+++ python-wsgi/test_mywsgi.py
@@ -9,3 +9,13 @@ class Test_wsgi_app(object):
         assert resp.content_type == 'text/html'
         s = resp.body.decode()
         assert s.startswith('Hello world:')
+
+    def test_post(self):
+        app= TestApp(mywsgi.application)
+        resp = app.post_json('/',
+                        { "id": 123, "flag": True })
+        assert resp.status_code == 200
+        assert resp.content_type == 'text/html'
+        s = resp.body.decode()
+        print(s)
+        assert s.startswith('Hello world:')
