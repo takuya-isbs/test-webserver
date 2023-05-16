000c11cdcc4afd3ae94654164cfc3dcc1c332ff1
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2023-05-16 15:00:51 +0900

    rust: uninstall program from /usr/local/bin by root(uid=0)

--- rust/uninstall.sh
+++ rust/uninstall.sh
@@ -4,7 +4,11 @@ set -eux
 
 . ./common.sh
 
-cargo uninstall --package $NAME
+if [ $uid -eq 0 ]; then
+    cargo uninstall --root /usr/local --package $NAME
+else
+    cargo uninstall --package $NAME
+fi
 systemctl stop $SYSTEMD_OPT_USER ${NAME}.service
 systemctl disable $SYSTEMD_OPT_USER ${NAME}.service
 rm -f ${SYSTEMD_SYSTEM_DIR}/${NAME}.service ${ENVFILE}
