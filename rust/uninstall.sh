8962d33657a59bf80433ffbc23038bd85ef25cc7
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2023-05-16 12:59:10 +0900

    rust:uninstall.sh: stop rustweb

--- rust/uninstall.sh
+++ rust/uninstall.sh
@@ -5,5 +5,6 @@ set -eux
 . ./common.sh
 
 cargo uninstall --package $NAME
+systemctl stop $SYSTEMD_OPT_USER ${NAME}.service
 systemctl disable $SYSTEMD_OPT_USER ${NAME}.service
 rm -f ${SYSTEMD_SYSTEM_DIR}/${NAME}.service ${ENVFILE}
